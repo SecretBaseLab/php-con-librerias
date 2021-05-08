9a616bfe5c22bc19649c749227eadc708e8ec311
Darwin Bayas <tidomar@gmail.com>
2021-05-08 15:08:41 -0500

    corrigiendo el driver de la base de datos

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 $capsule = new Capsule;
 $capsule->addConnection([
-    'driver'    => $_ENV['DB_DRIVE'],
+    'driver'    => $_ENV['DB_DRIVER'],
     'host'      => $_ENV['DB_HOST'],
     'database'  => $_ENV['DB_NAME'],
     'username'  => $_ENV['DB_USER'],
